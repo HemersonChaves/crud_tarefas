30357a7dfc0289c45c4b864285daa1a3e821d1b3
HemersonChaves <hemersonchaves20@hotmail.com>
2021-07-28 07:31:24 -0300

    feature inserir novo usuario

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4, validate } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
@@ -16,6 +15,16 @@ function checksExistsUserAccount(request, response, next) {
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const {name, username} = request.body;
+  const id_ = uuidv4();
+  users.push({ 
+    id: id_,
+    name: name, 
+    username: username, 
+    todos: []
+  });
+  return response.status(201).json(users.find(user => user.id === id_));
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
